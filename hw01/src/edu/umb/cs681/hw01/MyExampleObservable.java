04c08b5005fb12e35b6b403e90c7c7c07e9cc9f9
MounikaChava304 <62025664+MounikaChava304@users.noreply.github.com>
2020-10-27 15:47:22 -0400

    CS681 HomeWork

---
+++ hw01/src/edu/umb/cs681/hw01/MyExampleObservable.java
@@ -0,0 +1,27 @@
+package edu.umb.cs681.hw01;
+
+public class MyExampleObservable {
+	public static void main(String args[]) {
+
+		StockQuoteObservable stockQuote = new StockQuoteObservable();
+		stockQuote.addObserver((Observable o, Object obj) -> {
+			System.out.println("Stock Observer 1 notified.");
+		});
+		stockQuote.changeQuote("Google", 1);
+		stockQuote.addObserver((Observable o, Object obj) -> {
+			System.out.println("Stock Observer 2 notified.");
+		});
+		stockQuote.changeQuote("Apple", 2);
+
+		DJIAQuoteObservable djiaQuote = new DJIAQuoteObservable();
+		djiaQuote.addObserver((Observable o, Object obj) -> {
+			System.out.println("DJIA Observer 1 notified.");
+		});
+		djiaQuote.addObserver((Observable o, Object obj) -> {
+			System.out.println("DJIA Observer 2 notified.");
+		});
+		djiaQuote.changeQuote("ABCD", 143);
+		djiaQuote.changeQuote("EFGH", 33);
+
+	}
+}
