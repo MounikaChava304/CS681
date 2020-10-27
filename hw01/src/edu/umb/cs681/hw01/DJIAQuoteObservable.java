c25903698f94a482c000041eccf68b3d0058c282
MounikaChava304 <62025664+MounikaChava304@users.noreply.github.com>
2020-10-27 15:45:56 -0400

    delete

--- hw01/src/edu/umb/cs681/hw01/DJIAQuoteObservable.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs681.hw01;
-
-public class DJIAQuoteObservable extends Observable {
-	void changeQuote(String t, float q) {
-		setChanged();
-		notifyObservers(new DJIAEvent(q));
-	}
-}
--- hw01/src/edu/umb/cs681/hw01/Observer.java
+++
@@ -1,6 +0,0 @@
-package edu.umb.cs681.hw01;
-
-@FunctionalInterface
-public interface Observer {
-	void update(Observable observable, Object object);
-}
